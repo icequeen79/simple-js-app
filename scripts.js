dbe69e1816670ed416efc5a9d03e353576cd7e18
icequeen79 <87491011+icequeen79@users.noreply.github.com>
2021-08-30 12:45:42 -0700

    started a pokedex / pokemon list

--- scripts.js
+++ scripts.js
@@ -1,3 +1,33 @@
-alert('Hello world');
-let favoriteFood= 'Sushi';
-document.write(favoriteFood);
+//List of pokemon
+let pokemonList = [
+  {
+    name: 'Charmander',
+    height: 2',
+    types: ['Lizard', 'Blaze']
+  },
+  {
+    name: 'Squirtle',
+    height: 1'08",
+    types: ['Tiny Turtle', 'Torrent']
+  },
+  {
+    name: 'Vulpix',
+    height: 2',
+    types: ['Fox', 'Flash Fire']
+  },
+  {
+    name: 'Oddish',
+    height: 1'08",
+    types: ['Weed', 'Chlorophyll']
+  },
+  {
+    name: 'Psyduck',
+    height: 2'07",
+    types: ['Duck', 'Damp', 'Cloud Nine']
+  },
+  {
+    name: 'Hypno',
+    height: 5'03",
+    types: ['Hypnosis', 'Insomnia', 'Forewarn']
+  },
+];

44c6927de28ac52defbace5d02d35401c14dc82c
icequeen79 <87491011+icequeen79@users.noreply.github.com>
2021-08-30 19:02:38 -0700

    adjusted the height

--- scripts.js
+++ scripts.js
@@ -2,32 +2,32 @@
 let pokemonList = [
   {
     name: 'Charmander',
-    height: 2',
+    height: '2',
     types: ['Lizard', 'Blaze']
   },
   {
     name: 'Squirtle',
-    height: 1'08",
+    height: '1',
     types: ['Tiny Turtle', 'Torrent']
   },
   {
     name: 'Vulpix',
-    height: 2',
+    height: '2',
     types: ['Fox', 'Flash Fire']
   },
   {
     name: 'Oddish',
-    height: 1'08",
+    height: '1',
     types: ['Weed', 'Chlorophyll']
   },
   {
     name: 'Psyduck',
-    height: 2'07",
+    height: '2',
     types: ['Duck', 'Damp', 'Cloud Nine']
   },
   {
     name: 'Hypno',
-    height: 5'03",
+    height: '5',
     types: ['Hypnosis', 'Insomnia', 'Forewarn']
   },
 ];

a8f21f806286d7ac50a9acd6f832799bc5c5a618
icequeen79 <87491011+icequeen79@users.noreply.github.com>
2021-09-01 09:11:30 -0700

    removed quotes from height, wrote new conditions

--- scripts.js
+++ scripts.js
@@ -2,40 +2,41 @@
 let pokemonList = [
   {
     name: 'Charmander',
-    height: '2',
+    height: 2,
     types: ['Lizard', 'Blaze']
   },
   {
     name: 'Squirtle',
-    height: '1',
+    height: 1,
     types: ['Tiny Turtle', 'Torrent']
   },
   {
     name: 'Vulpix',
-    height: '2',
+    height: 2,
     types: ['Fox', 'Flash Fire']
   },
   {
     name: 'Oddish',
-    height: '1',
+    height: 1,
     types: ['Weed', 'Chlorophyll']
   },
   {
     name: 'Psyduck',
-    height: '2',
+    height: 2,
     types: ['Duck', 'Damp', 'Cloud Nine']
   },
   {
     name: 'Hypno',
-    height: '5',
+    height: 5,
     types: ['Hypnosis', 'Insomnia', 'Forewarn']
   },
 ];
 
-for (let i=0; i < pokemonList.length; i++) {
-if (pokemonList[i].length > 0){
-  document.write({pokemonList.name}, {pokemonList.height});
-} else (pokemonList[i].length < 10){
-  document.write("More info coming soon!");
+for (let i=0; i < pokemonList.length - 1; i++) {
+if (pokemonList[i].height > 1) {
+  document.write(pokemonList.name + ' ' + pokemonList[i].height);
+  document.write('</br>')
+} else {
+  document.write("More info coming soon!</br>");
 }
 }
